57197ec0ed20e4b2f7ad5ee22c7a78023721faa2
liguang <liguanghe09@gmail.com>
2020-11-24 10:07:18 +0100

    update main mlx

--- study/StateUpdate_dt.m
+++ study/StateUpdate_dt.m
@@ -1,4 +1,4 @@
-function [state, u] = StateUpdate_dt(state0, u_steer_dis, t, dt, v_last_iter)
+function [state, steer_list, f_dri_list, burn_list, iteration] = StateUpdate_dt(state0, u_steer_dis, t, dt, v_last_iter)
 
 % initialize
 L=1.516;
@@ -6,10 +6,16 @@ m= 210;
 v_gear= 20/3.6;
 v_off= 25/3.6;
 v_on= 15/3.6;
-f_dri_scale= 500;
+f_dri_scale= 1000;
 
 state=[];
 u=[];
+
+f_dri_list=[];
+burn_list=[];
+steer_list= [];
+
+iteration= 1;
 
 % iterate steer angle
 for j= 1: length(u_steer_dis)
@@ -25,22 +31,36 @@ for j= 1: length(u_steer_dis)
         % Engine on & off
         if (v<= v_on || (v_on< v <v_off && v>v_last_iter))
             burn= 1;
+            iteration= iteration+1;
+            
+            burn_list= [burn_list, burn];
         end
         
         if (v>= v_off || (v_on<v <v_off && v<v_last_iter))
             burn= 0;
+            burn_list= [burn_list, burn];
         end
+        
+        
         
         % Select torque vs speed
         if burn== 1
             if (v< v_gear)
                 f_dri= f_dri_scale;
-            elseif (v>=v_gear && v<v_off)
+            end
+            
+            if (v>=v_gear && v<v_off)
                 f_dri= 0.5* f_dri_scale;
             end
-        else
-            f_dri= 0;
+            f_dri_list= [f_dri_list, f_dri/f_dri_scale];
         end
+        
+        if burn== 0
+            f_dri= 0;
+            f_dri_list= [f_dri_list, f_dri/f_dri_scale];
+        end
+        
+        
         
         % initialize 
         v_tmp= v;
@@ -48,7 +68,7 @@ for j= 1: length(u_steer_dis)
         
         % resistance & x dimension effort
         f_drag= 0.8*1.17*1.04*v_tmp^2*1;
-        fx= (-f_drag*L/2)* cos(steer)+ f_dri- f_drag*L/2;
+        fx= (-f_drag/2)* cos(steer)+ f_dri- f_drag/2;
         
         % Update states
         theta= theta_tmp+ v_tmp*tan(steer)/L *dt;
@@ -59,7 +79,7 @@ for j= 1: length(u_steer_dis)
         
         % store results
         state=[state, [j; x; y; theta; v]];
-        u=[u, [j; steer; f_dri/f_dri_scale; burn]];
+        steer_list= [steer_list, steer];
     end
 end
 
